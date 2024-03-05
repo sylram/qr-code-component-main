5657b3aeb9e9a7c2fac7b60343d0d986d90607c5
Marlys Alvarado <sylram@icloud.com>
2024-03-05 16:09:15 -0500

    aria labels , landmark roles

---
+++ test.js
@@ -0,0 +1,44 @@
+console.log(2+4);
+console.log('test');
+
+let age = "salad";
+
+if(age >= 65) {
+    console.log("You get your income from your pension");
+} else if ( age >= 18){
+    console.log("Each month you get a salary");
+} else if ( age < 18){
+    console.log("You get an allowance");
+} else {
+    console.log("The value of the age variable is not numerical");
+}
+
+let day = "Sunda";
+
+switch(day){
+    case "Monday":
+     console.log("Do something");
+     break;
+    case "Tuesday":
+     console.log("Do something");
+     break;
+    case "Thursday":
+     console.log("Do something");
+     break;
+    case "Friday":
+     console.log("Do something");
+     break;
+    case "Saturday":
+     console.log("Do something");
+     break;
+     case "Sunday":
+        console.log("Do something");
+        break;
+    default:
+        console.log("There is no such day");
+
+    
+}
+
+
+
